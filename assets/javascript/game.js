dc58c1d5747d6d6b1de07020dcf8e2264c9c06d5
Alyssa Szutz <aaszutz@gmail.com>
2019-02-12 15:08:50 -0500

    added code

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//make variables for number to generate
+var numberArray = [100, 110, 115, 125, 130, 135, 140, 150, 175, 200];
+var generatedNumber = numberArray[Math.floor(Math.random() * numberArray.length)];
+
+$("#generatedNumber").text(generatedNumber);
+
+var counter = 0;
+
+//make the buttons each have a value
+
+
+//make and if then statements for the totals
+
+//make the total of your guesses go up and down
+
+//if the total guess exceeds the generated number make loss counter go up
+
+//if the total guess equals the generated number make the wins counter go up
+
